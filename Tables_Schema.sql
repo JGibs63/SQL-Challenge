2ef040e5e343f0a3aea8b0da6fab1da0b8753094
Josh Gibson <jag.gibson95@gmail.com>
2023-11-04 09:56:00 -0500

    Finished Query, updated Schema

---
+++ Tables_Schema.sql
@@ -0,0 +1,34 @@
+CREATE TABLE titles(
+	title_id VARCHAR,
+	title VARCHAR NOT NULL
+);
+
+CREATE TABLE departments(
+	dept_no VARCHAR,
+	dept_name VARCHAR
+);
+
+CREATE TABLE employees(
+	emp_no INT,
+	emp_title_id VARCHAR NOT NULL,
+	birth_date VARCHAR NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
+	sex VARCHAR NOT NULL,
+	hire_date VARCHAR NOT NULL,
+);
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+);
+
+CREATE TABLE salaries(
+	emp_no INT,
+	salary INT NOT NULL,
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
+);
